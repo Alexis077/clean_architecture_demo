496ca627c0cd08ff0e0fb50ee078590fdcd2458e
Alexis Urtecho Ocampo <alexisurtecho.au@gmail.com>
2025-04-18 02:32:29 -0600

    Remove unused import of BookExternalDto from BookApiInterface, streamlining the code and improving maintainability.

--- src/domain/repositories/book-api.interface.ts
+++ src/domain/repositories/book-api.interface.ts
@@ -1,5 +1,4 @@
 import { Book } from '../entities/book.entity';
-import { BookExternalDto } from '../../application/dtos/book.dto';
 
 export interface BookApiInterface {
   searchBooks(query: string): Promise<Book[]>;
